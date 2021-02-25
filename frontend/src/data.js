197f4e027e9e7ce4cb13d855067dd3eb64580df1
điệp <hdiep2705@gmail.com>
2021-02-25 22:28:50 +0700

    update code

---
+++ frontend/src/data.js
@@ -0,0 +1,83 @@
+const data =    {
+    products:[
+        {
+            _id: '1',
+            name: 'Xiaomi note 7',
+            category: 'phone',
+            image:'/images/redmi-note-7.jpg',
+            price: 100,
+            countInStock: 10,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality product'
+
+        },
+        {
+            _id: '2',
+            name: 'Xiaomi note 8',
+            category: 'phone',
+            image:'/images/redmi-note-8.jpg',
+            price: 150,
+            countInStock: 20,
+            brand: 'Xiaomi',
+            rating: 4.0,
+            numReviews: 10,
+            description:'high quality product'
+
+        },
+        {
+            _id: '3',
+            name: 'Xiaomi note 9',
+            category: 'phone',
+            image:'/images/redmi-note-9.jpg',
+            price: 220,
+            countInStock: 0,
+            brand: 'Xiaomi',
+            rating: 4.8,
+            numReviews: 17,
+            description:'high quality product'
+
+        },
+        {
+            _id: '4',
+            name: 'Xiaomi note 10',
+            category: 'phone',
+            image:'/images/redmi-note-10.jpg',
+            price: 350,
+            countInStock: 15,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 14,
+            description:'high quality product'
+
+        },
+        {
+            _id: '5',
+            name: 'Xiaomi note 11',
+            category: 'phone',
+            image:'/images/redmi-note-11.jpg',
+            price: 240,
+            countInStock: 5,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality product'
+
+        },
+        {
+            _id: '6',
+            name: 'Xiaomi mi 8',
+            category: 'phone',
+            image:'/images/redmi-mi-8.jpg',
+            price: 139,
+            countInStock: 12,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 15,
+            description:'high quality product'
+
+        }, 
+    ]
+}
+export default data;
